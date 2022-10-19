a821fcf460d3b2a97608dea52e3f63601a29176d
mahbub095 <mahbub.095@gmail.com>
2022-10-19 22:14:04 +0600

    permission test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Gate;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        Gate::authorize('access-dashboard');
         return view('admin.pages.dashboard');
     }
 
